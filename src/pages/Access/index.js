1460962c7685c773189f87f81d3cef1728ef3e3d
devprogramador <sjosefabio1996@gmail.com>
2023-05-09 23:46:00 -0300

    installing dependencies and organizing folders

---
+++ src/pages/Access/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text} from 'react-native';
+
+import { Container } from "./styles";
+
+function Access(){
+    return(
+        <Container>
+            <Text>Screen Access</Text>
+        </Container>
+    )
+}
+
+export default Access;
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text} from 'react-native';
+
+import { Container } from "./styles";
+
+function SignUp(){
+    return(
+        <Container>
+            <Text>Screen Access</Text>
+        </Container>
+    )
+}
+
+export default SignUp;
